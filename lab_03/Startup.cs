e276ba8e02bd4547b807ae0db6bd8082583ab451
ha280620 <77819575+ha280620@users.noreply.github.com>
2021-06-25 11:59:28 +0700

    khoi tao du an

---
+++ lab_03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab_03.Startup))]
+namespace lab_03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
